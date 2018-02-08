da81cec508a1824d5444aa56fbdf82dba763e94b
Tamsen1995 <Tamanhbui95@gmail.com>
2018-02-08 21:02:58 +0100

    [UPD] Some fixes and additions to the UI in the chat view controller.

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -42,15 +42,17 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
         
-        let messageArray = ["First Message", "Second Message", "Third Message"] // modify this
-        cell.messageBody.text = messageArray[indexPath.row]
+        cell.messageBody.text = messageArray[indexPath.row].messageBody
+        cell.senderUsername.text = messageArray[indexPath.row].sender
+        cell.avatarImageView.image = UIImage(named: "egg")
+        
         return cell
         
     }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return messageArray.count
     }
     
     // Whenever the table view within the chat viewcontroller is tapped
@@ -136,7 +138,11 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
             let message = Message()
             message.messageBody = text
             message.sender = sender
+            
             self.messageArray.append(message)
+            
+            self.configureTableView()
+            self.messageTableView.reloadData()
             
         }
     }
